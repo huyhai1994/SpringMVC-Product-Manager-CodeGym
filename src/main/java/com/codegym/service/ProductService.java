15da9e53a5d478cb63e99d3008dd2312637b8ffa
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-06-15 11:33:50 +0700

    lenovo_huyhai1994 Project product management Spring mvc with thymleaf

---
+++ src/main/java/com/codegym/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.codegym.service;
+
+import com.codegym.model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductService implements IProductService {
+    public static final Map<Integer, Product> products;
+
+    static {
+        products = new HashMap<Integer, Product>();
+        products.put(1, new Product(1, "samsung20", "Black", 20000000, 20));
+        products.put(2, new Product(2, "iphone10", "Yellow", 25000000, 10));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        products.put(product.getId(), product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        products.put(id, product);
+    }
+
+    @Override
+    public void remove(int id) {
+        products.remove(id);
+    }
+}
---
+++ src/test/java/com/codegym/service/ProductServiceTest.java
@@ -0,0 +1,13 @@
+package com.codegym.service;
+
+import com.codegym.model.Product;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static com.codegym.service.ProductService.products;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProductServiceTest {
+
+}
